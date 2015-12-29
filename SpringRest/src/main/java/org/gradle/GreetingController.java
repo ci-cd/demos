3e8b9c93202e22c8f6c8973f3b0e56956d0c5f70
Koti Reddy Mulumudi <kotireddy.mulumudi@gmail.com>
2015-12-29 14:05:20 -0600

    Spring Rest service initial checkin

---
+++ SpringRest/src/main/java/org/gradle/GreetingController.java
@@ -0,0 +1,21 @@
+package org.gradle;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(method = RequestMethod.GET, value = "greeting/{name}")
+    public Greeting greeting(@PathVariable String name){
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
