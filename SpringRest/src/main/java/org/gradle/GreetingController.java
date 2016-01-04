990b0025e70ddc6518fc155f2a599c8e8fc7fcd6
Koti Reddy Mulumudi <kotireddy.mulumudi@gmail.com>
2016-01-04 12:28:27 -0600

    implemented getDBData

--- SpringRest/src/main/java/org/gradle/GreetingController.java
+++ SpringRest/src/main/java/org/gradle/GreetingController.java
@@ -2,7 +2,10 @@ package org.gradle;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
@@ -23,6 +26,7 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
     
+        
     @Autowired
     CloudConfiguration config;
 
@@ -60,7 +64,70 @@ public class GreetingController {
             
         }
        
-        out.println(" Oracle DataSource : " + config.oracleDataSource());
 
+        out.println(" Oracle DataSource : " + config.oracleDataSource());
+        
+//        Connection con = config.oracleDataSource().getConnection();
+//        
+//        out.println(" Connection  : " + con);
+
+    }
+    
+    @RequestMapping("/getDBData")
+    public void getDBData(HttpServletResponse response) throws IOException, SQLException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1> Table Data </h1>");
+        
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+           conn = config.oracleDataSource().getConnection();
+           stmt = conn.createStatement();
+
+           String sql = "SELECT EMP_ID,EMP_NAME,EMP_DEPT_ID,EMP_LOC,EMP_SAL FROM Employee";
+          
+           ResultSet rs = stmt.executeQuery(sql);
+
+           out.println("<table><tr><th>EMP_ID</th><th>EMP_NAME</th><th>DEPT_ID</th><th>EMP_LOC</th><th>EMP-SAL<th></tr>");
+           //STEP 5: Extract data from result set
+           while(rs.next()){
+              //Retrieve by column name
+              int id  = rs.getInt("EMP_ID");
+              String name = rs.getString("EMP_NAME");
+              int deptId = rs.getInt("EMP_DEPT_ID");
+              String location = rs.getString("EMP_LOC");
+              int sal  = rs.getInt("EMP_SAL");
+             
+              out.println("<tr><td>" + id + "</td>" + "<td>" + name + "</td>" + "<td>" + deptId + "</td>" + "<td>" + location + "</td>" + "<td>" + sal + "</td></tr>");
+           }
+           
+           out.println("</table></body></html>");
+           //STEP 6: Clean-up environment
+           rs.close();
+           stmt.close();
+           conn.close();
+        }catch(SQLException se){
+           //Handle errors for JDBC
+           se.printStackTrace();
+        }catch(Exception e){
+           //Handle errors for Class.forName
+           e.printStackTrace();
+        }finally{
+           //finally block used to close resources
+           try{
+              if(stmt!=null)
+                 stmt.close();
+           }catch(SQLException se2){
+           }// nothing we can do
+           try{
+              if(conn!=null)
+                 conn.close();
+           }catch(SQLException se){
+              se.printStackTrace();
+           }//end finally try
+        }//end try
+        
     }
 }
